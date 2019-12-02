69b9fdcaef3e2d6b39a97b3c294b430412c45a90
blackrose9 <sarah.mwendwa@gmail.com>
2019-12-02 08:29:03 +0300

    gender now displaying

--- js/main.js
+++ js/main.js
@@ -8,67 +8,69 @@ var weeks = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "
 function compute(){
   var date = document.forms["form"]["date"].value;
   var newdate = new Date (date); 
-  // var ndate = Date.parse(date);
-  alert (date);
-  alert (newdate);
+  // alert (date);
+  // alert (newdate);
+   var weekday = newdate.getDay();
+  // alert (birthdate);
 
-  // var date1 = (date.toDateString());
-   var birthdate = newdate.getDay();
-  alert (birthdate);
+  //gender
+  var genderinput = document.getElementById("gender");
+  var gender = genderinput.options[genderinput.selectedIndex].text;
+  // alert(gender);
 }
 
 // Sunday - Saturday : 0 - 6
 
-// function akanName (weekday, gender){
-// //check male names
-// 	if (weekday === 0 && gender === "male"){
-//   	return ("Kwasi")
-//   }
-//   if (weekday === 1 && gender === "male"){
-//   	return ("Kwando")
-//   }
-//   if (weekday === 2 && gender === "male"){
-//   	return ("Kwabena")
-//   }
-//   if (weekday === 3 && gender === "male"){
-//   	return ("Kwaku")
-//   }
-//   if (weekday === 4 && gender === "male"){
-//   	return ("Yaw")
-//   }
-//   if (weekday === 5 && gender === "male"){
-//   	return ("Kofi")
-//   }
-//   if (weekday === 6 && gender === "male"){
-//   	return ("Kwame")
-//   }
-//   //check female names
-//   if (weekday === 0 && gender === "female"){
-//   	return ("Akosua")
-//   }
-//   if (weekday === 1 && gender === "female"){
-//   	return ("Adwoa")
-//   }
-//   if (weekday === 2 && gender === "female"){
-//   	return ("Abenaa")
-//   }
-//   if (weekday === 3 && gender === "female"){
-//   	return ("Akua")
-//   }
-//   if (weekday === 4 && gender === "female"){
-//   	return ("Yaa")
-//   }
-//   if (weekday === 5 && gender === "female"){
-//   	return ("Afua")
-//   }
-//   if (weekday === 6 && gender === "female"){
-//   	return ("Ama")
-//   }
-//   //catches everything else which isn't a date.
-//   else {
-//   	return("Invalid Date")
-//   }
-// }
+function akanName (weekday, gender){
+//check male names
+	if (weekday === 0 && gender === "male"){
+  	return ("Kwasi")
+  }
+  if (weekday === 1 && gender === "male"){
+  	return ("Kwando")
+  }
+  if (weekday === 2 && gender === "male"){
+  	return ("Kwabena")
+  }
+  if (weekday === 3 && gender === "male"){
+  	return ("Kwaku")
+  }
+  if (weekday === 4 && gender === "male"){
+  	return ("Yaw")
+  }
+  if (weekday === 5 && gender === "male"){
+  	return ("Kofi")
+  }
+  if (weekday === 6 && gender === "male"){
+  	return ("Kwame")
+  }
+  //check female names
+  if (weekday === 0 && gender === "female"){
+  	return ("Akosua")
+  }
+  if (weekday === 1 && gender === "female"){
+  	return ("Adwoa")
+  }
+  if (weekday === 2 && gender === "female"){
+  	return ("Abenaa")
+  }
+  if (weekday === 3 && gender === "female"){
+  	return ("Akua")
+  }
+  if (weekday === 4 && gender === "female"){
+  	return ("Yaa")
+  }
+  if (weekday === 5 && gender === "female"){
+  	return ("Afua")
+  }
+  if (weekday === 6 && gender === "female"){
+  	return ("Ama")
+  }
+  //catches everything else which isn't a date.
+  else {
+  	return("Invalid Date")
+  }
+}
 
 // console.log(akanName(weekday, gender)); 
 
